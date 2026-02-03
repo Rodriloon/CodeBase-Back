e0a966e472d8e56ac618d877be0681a7c54c48d6
nicoarditi <nicoarditi12@gmail.com>
2026-02-03 00:11:33 -0300

    Ignorar unused en GlobalExceptionHandler

--- src/main/java/CodeBase/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/CodeBase/demo/exception/GlobalExceptionHandler.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@SuppressWarnings("unused")
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
